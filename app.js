cc5a618a55984e097d15f4e93698234ad9c8b94f
Russ <rperry1886@gmail.com>
2020-09-29 23:37:22 -0400

    Add grabber to resize box

--- app.js
+++ app.js
@@ -1,28 +1,106 @@
 // Elements
 const container = document.getElementById('container');
+const xPos = document.getElementById('xPos');
+const yPos = document.getElementById('yPos');
+const elClicked = document.getElementById('clickedEl');
+let divs;
+let grabber;
+let txt;
 
 let down = false;
-let mousemoving = false;
+let elementsArray = [];
+let count = 0;
+let xMousePos = 0;
+let yMousePos = 0;
+let newDiv = false;
 
 // Create Div
 function createDiv(e) {
-  container.innerHTML = ''; // I know this isn't the best practice, or particularly safe, but it works for this for now.
-  const div = document.createElement('div');
-  div.style.width = '100%';
-  div.style.height = `${e.offsetY}px`;
-  div.innerText = '';
-  container.appendChild(div);
-  if (down) {
+  if (!newDiv) {
+    // newDiv makes it so only one div is created at once.
+    newDiv = true;
+
+    // Create the div itself
+    const div = document.createElement('div');
+    div.style.width = `${100}%`;
     div.style.height = `${e.offsetY}px`;
+    div.setAttribute('id', `div${count}`);
+
+    // Create the information text within div
+    txt = document.createElement('p');
+    txt.innerText = `( ${div.style.width}, ${div.style.height} )`;
+
+    // Create the grabber
+    grabber = document.createElement('div');
+    grabber.setAttribute('id', 'grabber');
+    grabber.innerText = ' ';
+
+    // Append items to the div
+    div.appendChild(txt);
+    div.appendChild(grabber);
+    container.appendChild(div);
+    if (down) {
+      div.style.height = `${e.offsetY}px`;
+    }
+
+    // Push element created to array
+    // elementsArray.push({
+    //   key: `div${count}`,
+    //   height: `${div.style.height}`,
+    //   width: `${div.style.width}`,
+    // });
+
+    count++;
+    divs = document.querySelectorAll('#container div');
+    addEventListenerList(divs, 'click', clickedDiv);
+
+    grabber.addEventListener('mousemove', updatePos);
+  } else {
+    return;
   }
 }
 
-container.addEventListener('click', createDiv);
-container.addEventListener('mousedown', () => {
-  down = true;
-  container.addEventListener('mousemove', createDiv);
-});
-container.addEventListener('mouseup', () => {
-  down = false;
-  container.removeEventListener('mousemove', createDiv);
-});
+// Add event listeners to each of the divs (currently there is only one div)
+function addEventListenerList(list, event, fn) {
+  for (var i = 0, len = list.length; i < len; i++) {
+    list[i].addEventListener(event, fn, false);
+  }
+}
+
+function updatePos(e) {
+  // Put positions in variables
+  xMousePos = e.offsetX;
+  yMousePos = e.offsetY;
+
+  // Update items within bottom left thing
+  xPos.innerText = `${xMousePos}`;
+  yPos.innerText = `${yMousePos}`;
+
+  // Update the div
+  if (e.which === 1) {
+    e.target.parentElement.style.height = `${e.pageY - 20}px`;
+    txt.innerText = `( ${e.target.parentElement.style.width}, ${e.target.parentElement.style.height} )`;
+  }
+}
+
+function clickedEl(e) {
+  elClicked.innerText = `${e.target.id}`;
+}
+
+function clickedDiv(e) {
+  console.log(e.target.id);
+  e.target.style.background = 'blue;';
+}
+
+window.addEventListener('pointerdown', createDiv);
+// document.addEventListener('mousemove', updatePos);
+document.addEventListener('click', clickedEl);
+
+// container.addEventListener('mousedown', () => {
+//   down = true;
+//   container.addEventListener('mousemove', createDiv);
+// });
+// container.addEventListener('mouseup', () => {
+//   down = false;
+//   container.removeEventListener('mousemove', createDiv);
+// });
